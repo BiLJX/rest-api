ec7757c93ceab775c40eef810d5cdf19c1018384
BiLJX <77322992+BiLJX@users.noreply.github.com>
2021-01-12 14:37:09 +0545

    Delete server directory

--- server/Algo/subAlgo/similarity.js
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase"
-const db = firebase.database()
-
-
-function Similarity(uID){
-    db.ref("users"+uID + "private/" )
-}
--- server/Modules/Ranking.js
+++
@@ -1,3 +0,0 @@
-function Ranking(data){
-    
-}
