533033e4eb54809f9b8d1eb07bbb99b0638167a9
BiLJX <77322992+BiLJX@users.noreply.github.com>
2021-05-03 09:44:27 +0545

    Update putInfo.js

--- Modules/putInfo.js
+++ Modules/putInfo.js
@@ -4,7 +4,7 @@ import  {getAll} from "./find.js"
 function putInfo(tracks = [], uid, db)
 {
     const final = tracks.map(value=>{
-        value.uploader = getAll(value.info.userID, db)
+        value.uploader = getAllM(value.info.userID, db)
         value.hasLiked = value?.likedBy?.find(x=>x.uid === uid)?.liked||false
         return value
     })
